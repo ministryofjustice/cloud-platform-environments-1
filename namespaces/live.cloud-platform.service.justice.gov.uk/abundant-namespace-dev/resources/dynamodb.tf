b87b757570be8bc54420889ce1493cf71507ac51
Jack Stockley <jack.stockley@digital.justice.gov.uk>
2023-02-16 16:41:15 +0000

    test(namespace): adding mismatch namespaces in multiple file to test

--- namespaces/live.cloud-platform.service.justice.gov.uk/abundant-namespace-dev/resources/dynamodb.tf
+++ namespaces/live.cloud-platform.service.justice.gov.uk/abundant-namespace-dev/resources/dynamodb.tf
@@ -48,3 +48,17 @@ resource "kubernetes_secret" "example_two_dynamodb" {
     secret_access_key = module.example_team_dynamodb.secret_access_key
   }
 }
+
+resource "kubernetes_secret" "example_three_dynamodb" {
+  metadata {
+    name      = "example-team-dynamodb-output"
+    namespace = "abundant-namespace-production"
+  }
+
+  data = {
+    table_name        = module.example_team_dynamodb.table_name
+    table_arn         = module.example_team_dynamodb.table_arn
+    access_key_id     = module.example_team_dynamodb.access_key_id
+    secret_access_key = module.example_team_dynamodb.secret_access_key
+  }
+}
